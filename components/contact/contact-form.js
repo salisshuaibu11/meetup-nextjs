154094a34cdeba299eb7f0fee86e139f1aa47f33
salisu <salisshuaibu11@gmail.com>
2021-08-22 01:20:45 +0000

    Working on contact-form

---
+++ components/contact/contact-form.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+import classes from "./contact-form.module.css";
+import Notification from "../UI/notification";
+
+async function sendContactData(contactDetails) {
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(contactDetails),
+    headers: {
+      "Contect-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+}
+
+function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+  const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
+  const [requestError, setRequestError] = useState();
+
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+    }
+  });
+}
+
+export default ContactForm;
